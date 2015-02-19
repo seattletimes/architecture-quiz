ade503ab612c78542de2f3e281962479eb07dc74
Thomas Wilburn <news@thomaswilburn.net>
2015-02-18 16:22:11 -0800

    Encapsulate Quiz functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,3 @@
-var zoom = require("./zoom");
+var Quiz = require("./quiz");
 
-var template = require("./_question.html");
-var ich = require("icanhaz");
-ich.addTemplate("question", template);
-
-var viewport = document.querySelector(".viewport");
-
-var current = 0;
-
-var render = function() {
-  var spec = window.imageList[current];
-  viewport.innerHTML = ich.question({
-    large: spec.large.file,
-    small: spec.small.file
-  });
-};
-
-document.querySelector(".next").addEventListener("click", () => {
-  current++;
-  render();
-});
-
-render();
-
-document.body.addEventListener("click", e => {
-  if (!e.target.className.contains("small")) return;
-  var small = e.target;
-  var spec = window.imageList[current];
-  zoom(small, spec);
-});
+var q = new Quiz(window.quizData, window.imageList, ".viewport");
--- src/js/quiz.js
+++ src/js/quiz.js
@@ -1,8 +1,17 @@
-var Quiz = function(data, images) {
+var zoom = require("./zoom");
+
+var template = require("./_question.html");
+var ich = require("icanhaz");
+ich.addTemplate("question", template);
+
+var Quiz = function(data, images, view) {
+  this.view = typeof view == "string" ? document.querySelector(view) : view;
   this.qIndex = 0;
   this.questions = data;
   this.images = images;
   this.correct = 0;
+  this.bind();
+  this.render();
 };
 
 Quiz.prototype = {
@@ -10,18 +19,38 @@ Quiz.prototype = {
   questions: null,
   correct: null,
   render: function() {
-
+    var spec = this.images[this.qIndex];
+    this.view.innerHTML = ich.question({
+      large: spec.large.file,
+      small: spec.small.file
+    });
   },
   bind: function() {
-
+    var self = this;
+    document.querySelector(".next").addEventListener("click", this.next.bind(this));
+    document.body.addEventListener("click", function(e) {
+      if (!e.target.className.contains("small")) return;
+      self.shrink();
+    });
   },
   check: function() {
 
   },
   next: function() {
-
+    this.qIndex++;
+    if (this.qIndex >= this.questions.length) {
+      return this.complete();
+    }
+    this.render();
   },
   complete: function() {
-    
+    console.log("ALL DONE");
+  },
+  shrink: function() {
+    var small = document.querySelector(".small");
+    var spec = this.images[this.qIndex];
+    zoom(small, spec);
   }
 };
+
+module.exports = Quiz;
